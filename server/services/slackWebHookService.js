0204d3d7596b85ac6331336b1b72887ed2b1e183
Cristiano Oliveira <cristianodpoliveira@gmail.com>
2015-10-10 17:09:59 +0100

    Adds origin to a object

--- server/services/slackWebHookService.js
+++ server/services/slackWebHookService.js
@@ -6,34 +6,39 @@ slackWebHookService = {
 		var username        = payload.user_name;
 		var original_text   = payload.text;
 		var trigger_word    = payload.trigger_word;
-	
+
 		var data = original_text.split(trigger_word)[1].trim();
-		
-		var message = {
-			author: username,
-			origin: '#' + channel_name + ' @ ' + team_domain + '.slack.com',
-			timestamp: timestamp
-		};
-		
+
+		var origin,
+            origin.service      = "slack.com",
+            origin.channel_name = channel_name,
+            origin.team_domain  = team_domain;
+
+        var message = {
+            author: username,
+            origin: origin,
+            timestamp: timestamp
+        };
+
 		var messageType = this.getType(data);
-		
+
 		message.type = messageType.type;
-		message.data = messageType.data; 
-		
-		return message;	
+		message.data = messageType.data;
+
+		return message;
 	},
-	
+
 	getType : function(data) {
 		var youtubeRegex = /^.*(<).*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*(>)/;
 		var match = data.match(youtubeRegex);
-		
+
 		if (match&&match[8].length==11) {
 			return {
 				type : 'video/youtube',
 				data : match[8]
 			}
 		}
-		
+
 		var imageExtReged = /\.(jpeg|jpg|gif|png)>$/;
 		if (data.match(imageExtReged) != null) {
 			var urlRegex = /((<)(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?(>))/
@@ -42,10 +47,10 @@ slackWebHookService = {
 				data : data.match(urlRegex)[1].replace('<', '').replace('>', '')
 			}
 		}
-		
+
 		return {
 			type: 'text',
 			data: data
 		}
-	} 
+	}
 }
