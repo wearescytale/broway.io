68eb96b266a9c0bced88b0d51f5ee3c2f9949c2e
Eduardo Conceicao <eduardo.conceicao@gmail.com>
2015-10-10 19:52:24 +0100

    Added slack integrations settings

--- client/templates/settings/settings.js
+++ client/templates/settings/settings.js
@@ -1,3 +1,14 @@
 Router.route('/settings', function () {
-    this.render('settings');
+    this.layout('settings');
+    this.render('slack');
+});
+
+Router.route('/settings/meetup', function () {
+    this.layout('settings');
+    this.render('meetup');
+});
+
+Router.route('/settings/slack', function () {
+    this.layout('settings');
+    this.render('slack');
 });
