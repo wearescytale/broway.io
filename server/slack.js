431afa3e020ffff1419ad0d6af75747fb9e4361f
Eduardo Conceicao <eduardo.conceicao@gmail.com>
2015-10-10 20:34:22 +0100

    Added slack integration validation

--- server/slack.js
+++ server/slack.js
@@ -1,14 +1,20 @@
 Router.route('/webhooks/slack', { where: 'server' })
   .post(function () {
-    var request = this.request;
-    var payload = request.body;
-    var message = slackWebHookService.getMessage(payload);
-
-    Messages.insert(message);
-
+    var request  = this.request;
+    var payload  = request.body;
     var response = this.response;
+
+    slackSettings = Integrations.findOne({key: "slack"});
+
+    if (!payload.token || slackSettings.token !== payload.token || !slackSettings.enabled) {
+        response.writeHead(400);
+        response.end();
+        return;
+    };
+
+    var message = slackWebHookService.getMessage(payload);
+    Messages.insert(message);
 
     response.writeHead(204);
     response.end();
-
   });
